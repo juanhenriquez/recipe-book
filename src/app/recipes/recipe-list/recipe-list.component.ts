9e702c3d70cc3d8c9b5eb7f82a9d24fcde968427
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-10-19 02:56:03 -0400

    Add a fake recipe only for test purposes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe';
+
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [];
+  recipe: Recipe = new Recipe("Panquecas", "Descripcion de las panquecas", "https://goo.gl/oH31R4");
 
   constructor() { }
 
