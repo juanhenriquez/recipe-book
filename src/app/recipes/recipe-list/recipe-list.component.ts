b8a0fde17895d9929bd05ed61bba1dd6b4e2ae70
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-10-19 04:43:58 -0400

    Add an ouput EventListener for emit recipes to recipe component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output} from '@angular/core';
 
 import { Recipe } from '../recipe';
 
@@ -11,10 +11,15 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [];
   recipe: Recipe = new Recipe("Panquecas", "Descripcion de las panquecas", "https://goo.gl/oH31R4");
+  @Output() selectRecipe = new EventEmitter<Recipe>();
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectedRecipe (recipe: Recipe) {
+    this.selectRecipe.emit(recipe);
   }
 
 }
