937a9e1d96e4892c9e28b8629d3bc2d6733ea117
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-10-19 04:49:39 -0400

    Add custom event binding for listen the selectRecipe EventEmitter

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from './recipe';
+
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.scss']
 })
 export class RecipesComponent implements OnInit {
+  
+  selectedRecipe: Recipe;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelectRecipe (recipe) {
+    this.selectedRecipe = recipe;
   }
 
 }
